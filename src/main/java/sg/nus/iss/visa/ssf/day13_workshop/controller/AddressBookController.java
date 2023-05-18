e0d19f8f4e6cc524c259af6c306f97d444623e15
russell258 <duckie258@gmail.com>
2023-05-19 02:22:49 +0800

    adding id to contact class

--- src/main/java/sg/nus/iss/visa/ssf/day13_workshop/controller/AddressBookController.java
+++ src/main/java/sg/nus/iss/visa/ssf/day13_workshop/controller/AddressBookController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import jakarta.validation.Valid;
 import sg.nus.iss.visa.ssf.day13_workshop.Day13WorkshopApplication;
 import sg.nus.iss.visa.ssf.day13_workshop.model.Contact;
--- src/main/java/sg/nus/iss/visa/ssf/day13_workshop/model/Contact.java
+++ src/main/java/sg/nus/iss/visa/ssf/day13_workshop/model/Contact.java
@@ -1,10 +1,14 @@
 package sg.nus.iss.visa.ssf.day13_workshop.model;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.Random;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -26,20 +30,54 @@ public class Contact {
     //one more for cannot be younger than 10 years old or older than 100... custom constraint?
     @NotNull(message = "Date of birth is mandatory")
     @Past(message = "Date of birth cannot be in the future")
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     private LocalDate dob;
 
+    @Min(value = 10, message = "Must be above 10 years old")
+    @Max(value = 100, message = "Must be below 100 years old")
+    private int age;
+
     //should have a private final string id with uuid generator to add into constructors too.
+    private String id;
 
     public Contact() {
+        this.id = generateId();
     }
 
-    public Contact(String name, String email, String phoneNumber, LocalDate dob) {
+    public Contact(String id, String name, String email, String phoneNumber, LocalDate dob) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.dob = dob;
     }
+
+    // any difference between this vs UUID?
+    private String generateId(){
+        Random r =  new Random();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() <8 ) {
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0,8);
+    }
     
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id=id;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
     public String getName() {
         return name;
     }
@@ -62,7 +100,14 @@ public class Contact {
         return dob;
     }
     public void setDob(LocalDate dob) {
-        this.dob = dob;
+        //calculate age
+        int calculatedAge=0;
+        if (dob!=null){
+            calculatedAge = Period.between(dob,LocalDate.now()).getYears();
+        }
+
+        this.age = calculatedAge;
+        this.dob=dob;
     }
 
     @Override
